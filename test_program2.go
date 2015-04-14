c92e205c44d16f9330c9c8fc725c09920109e384
Keheliya Gallaba <keheliya.gallaba@gmail.com>
2015-04-14 09:54:16 -0700

    testing scopes

---
+++ test_program2.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+var gx = "Goodbye"
+
+func main() {
+	x := "Hello"
+	fmt.Println(x)
+	fmt.Println(gx)
+	inc := func(x int) int {
+		var amt = 1
+		fmt.Println("inside incr")
+		return x + amt
+	}
+	fmt.Println(inc(2))
+	f()
+
+}
+
+func f() {
+	y := "Rocky"
+	fmt.Println(y)
+	fmt.Println(gx)
+}
